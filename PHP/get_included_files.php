a21b9f2807c25bb0f21df7e23407075923ecd9ad
getcrud@gmail.com <getcrud@gmail.com>
2017-06-06 23:43:14 +0700

    no message

--- PHP/get_included_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title = 'names of included or required files';
-include 'header.php';
-
-$files = get_included_files();
-echo '<pre>', print_r($files, true), '</pre>';
-fwrite(fopen('logs_get_included_files.txt', 'a+'), print_r($files, true));
-
-include 'footer.php';
