d5411a980bc4054a7e5e9d62975b2b2cd268480f
pitt phunsanit <phunsanit@gmail.com>
2019-05-25 23:26:57 +0700

    no message

--- PHP/download.php
+++ PHP/download.php
@@ -1,14 +1,23 @@
 <?php
-$fileDir = 'D:\xampp\htdocs\snippets\\';
+$fileDir = '../assets/';
 $token = 'HH89VOiirgXlCdEqDrFs';
 
-if ($_POST['token'] != $token) {
+if ($_REQUEST['token'] != $token) {
     exit('bad token');
 }
 
-$requestFile = $fileDir . $_POST['file'];
-if (file_exists($requestFile)) {
-    readfile($requestFile);
+$file = $fileDir . $_REQUEST['file'];
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: ' . mime_content_type($file));
+    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+
+    //readfile($file);
+    echo file_get_contents($file);
 } else {
     exit('file not found');
 }
