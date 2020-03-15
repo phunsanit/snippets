b90d91351d5f220cc4c8d133eab53a5424365825
pitt phunsanit <phunsanit@gmail.com>
2020-03-15 18:38:33 +0700

    no message

--- PHP/json.json_encode.php
+++ PHP/json.json_encode.php
@@ -1,12 +1,14 @@
 <?php
-$datas = [
-    "aaa" => "bbb",
-    "ccc" => "ddd",
-    "eee" => "fff",
-    "ggg" => "hhh",
+$results = [
+    'datas' => [
+        "aaa" => "bbb",
+        "ccc" => "ddd",
+        "eee" => "fff",
+        "ggg" => "hhh",
+    ],
 ];
 
-$datas = array_merge($datas, $_REQUEST);
+$datas = array_merge($results, $_REQUEST);
 
 header('Content-Type: application/json');
 echo json_encode($datas);
