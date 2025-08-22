4c43bd44feb41ba7cff2c68fefa2f5fcf414fbc5
Pitt Phunsanit <Phunsanit@gmail.com>
2025-08-22 22:13:45 +0700

    Tabulator: data.filters

---
+++ Tabulator/assets/Tabulator.helper.filter.js
@@ -0,0 +1,48 @@
+//https://tabulator.info/docs/6.3/filter
+
+//Define variables for input elements
+var fieldEl = document.getElementById("filter-field");
+var typeEl = document.getElementById("filter-type");
+var valueEl = document.getElementById("filter-value");
+
+//Custom filter example
+function customFilter(data) {
+    return data.car && data.rating < 3;
+}
+
+//Trigger setFilter function with correct parameters
+function updateFilter() {
+    var filterVal = fieldEl.options[fieldEl.selectedIndex].value;
+    var typeVal = typeEl.options[typeEl.selectedIndex].value;
+
+    // Decode the filter value
+    filterVal = decodeURIComponent(filterVal);
+
+    var filter = filterVal == "function" ? customFilter : filterVal;
+
+    if (filterVal == "function") {
+        typeEl.disabled = true;
+        valueEl.disabled = true;
+    } else {
+        typeEl.disabled = false;
+        valueEl.disabled = false;
+    }
+
+    if (filterVal) {
+        table.setFilter(filter, typeVal, valueEl.value);
+    }
+}
+
+//Update filters on value change
+document.getElementById("filter-field").addEventListener("change", updateFilter);
+document.getElementById("filter-type").addEventListener("change", updateFilter);
+document.getElementById("filter-value").addEventListener("keyup", updateFilter);
+
+//Clear filters on "Clear Filters" button click
+document.getElementById("filter-clear").addEventListener("click", function () {
+    fieldEl.value = "";
+    typeEl.value = "=";
+    valueEl.value = "";
+
+    table.clearFilter();
+});
