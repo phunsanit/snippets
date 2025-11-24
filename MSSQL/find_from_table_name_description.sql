f4303fc388c3b398718a96eb2aa01eb2c689b9c4
Pitt Phunsanit <Phunsanit@gmail.com>
2025-11-24 15:32:39 +0700

    find_from_table_name_description.sql

--- MSSQL/find_from_table_name_description.sql
+++ MSSQL/find_from_table_name_description.sql
@@ -12,30 +12,33 @@ SELECT
     CAST(ep_c.value AS NVARCHAR(MAX)) AS ColumnDescription,
 
     -- Combined Name for reference
-    QUOTENAME(DB_NAME()) + '.' + QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name) AS FullyQualifiedObjectName
+DB_NAME() + '.' + SCHEMA_NAME(t.schema_id) + '.' + t.name + '.' + c.name AS FullyQualifiedObjectName
 FROM
     sys.tables t
 INNER JOIN
     sys.columns c
-    ON t.object_id = c.object_id
+    ON
+t.object_id = c.object_id
 -- Join for TABLE Description (minor_id = 0)
 LEFT JOIN
     sys.extended_properties ep_t
-    ON t.object_id = ep_t.major_id
-    AND ep_t.minor_id = 0
-    AND ep_t.name = 'MS_Description'
+    ON
+t.object_id = ep_t.major_id
+AND ep_t.minor_id = 0
+AND ep_t.name = 'MS_Description'
 -- Join for COLUMN Description (minor_id = column_id)
 LEFT JOIN
     sys.extended_properties ep_c
-    ON c.object_id = ep_c.major_id
-    AND c.column_id = ep_c.minor_id
-    AND ep_c.name = 'MS_Description'
+    ON
+c.object_id = ep_c.major_id
+AND c.column_id = ep_c.minor_id
+AND ep_c.name = 'MS_Description'
 WHERE
     t.name LIKE @search
-    OR CAST(ep_t.value AS NVARCHAR(MAX)) LIKE @search
-    OR c.name LIKE @search
-    OR CAST(ep_c.value AS NVARCHAR(MAX)) LIKE @search
+OR CAST(ep_t.value AS NVARCHAR(MAX)) LIKE @search
+OR c.name LIKE @search
+OR CAST(ep_c.value AS NVARCHAR(MAX)) LIKE @search
 ORDER BY
-    SchemaName,
-    TableName,
-    ColumnOrder;
+    SchemaName
+    ,TableName
+    ,ColumnOrder;
